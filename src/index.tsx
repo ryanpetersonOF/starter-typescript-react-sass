f8351aee1f6a3f062e728cf248221755756e5ecd
ryanpetersonOF <ryan@openfin.co>
2019-12-09 11:14:08 +0000

    [N/A] Set Components in dirs

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom'
-import {Hello} from './components/Hello';
+import {Hello} from './components/Hello/Hello';
 
 ReactDOM.render(<Hello/>, document.getElementById('app'));
