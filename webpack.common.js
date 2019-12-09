43fe3226238ab01f2039eb88ebf58917e9becb1e
tim-dinsdale <48913869+tim-dinsdale@users.noreply.github.com>
2019-12-09 10:40:25 +0000

    Add files via upload

---
+++ webpack.common.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {
+    NODE_ENV = 'development',
+} = process.env;
+const isProduction = NODE_ENV === 'production';
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+           
+            {
+                test: /\.css$/,
+                use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'],
+            },
+            
+            {
+                test: /\.scss$/,
+                use: [
+                  isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      modules: true,
+                      localIdentName: '[name]__[local]___[hash:base64:5]',
+                      sourceMap: !isProduction
+                    }
+                  },
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      sourceMap: !isProduction
+                    }
+                  }
+               ],
+            }        
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.[hash]-[name].css',
+        }),
+        /*
+        new HtmlWebpackPlugin({
+            chunks: ['main', 'styles.[hash]-[name].css'],
+            env: NODE_ENV,
+            filename: 'index.html',
+            template: path.join(__dirname, 'dist', 'index.html'),
+        }),
+        */
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+    }    
+};
